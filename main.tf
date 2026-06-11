a5e3490a2e9c0c158e2f4d98c7b91645c75d588a
Vadivelu M <vadivelumsolo@gmail.com>
2026-06-12 00:56:20 +0530

    new changes

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ubuntu_terraform" {
   ami           = "ami-091138d0f0d41ff90"
   instance_type = "t3.micro"
-  key_name      = "k8s_key.pem"
+  key_name      = "k8s_key"
 
   tags = {
     Name = "UbuntuTerraform"
